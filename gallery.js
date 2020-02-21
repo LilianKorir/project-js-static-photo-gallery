176f18273d5db4a0840a78f65a4d06c269f59f9a
Lilian Korir <likorir@davidson.edu>
2020-02-20 16:57:05 -0800

    --photo

--- gallery.js
+++ gallery.js
@@ -2,12 +2,19 @@ let fs = require('fs');
 
 let imageFileNames = fs.readdirSync('site/images');
 
-// This code is just here to demonstrate how fs.readdirSync works
+//This code is just here to demonstrate how fs.readdirSync works
 
-console.log('sites/images contains the following files:')
-
+console.log('sites/images contains the following files:');
+let imageGallery;
 for (let i = 0; i < imageFileNames.length; i++) {
   let fileName = imageFileNames[i];
-
-  console.log(`${i + 1}. ${fileName}`);
+  if (i === 0) {
+    imageGallery = `<img src="images/${fileName}" height="400" width="550">`;
+  }
+  else {
+    imageGallery = imageGallery + `<img src="images/${fileName}" height="400"width="550" >`;
+  }
+  fs.writeFileSync('site/index.html', imageGallery, function (err) {
+    if (err) return console.log(err);
+  });
 }
